b2725f74a160bbfa615420ff0b3447ff2f47d7ba
unknown <manoj.kumar@nmbrsystems.com>
2019-06-11 18:51:47 +0530

    descending order function implemented on dashboard

--- src/app/pages/dashboard/components/topDetails/topDetails.component.ts
+++ src/app/pages/dashboard/components/topDetails/topDetails.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, Input, ViewChild} from '@angular/core';
-import {TopDetailsService} from './topDetails.service';
-import {Table} from 'primeng/table';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { TopDetailsService } from './topDetails.service';
+import { Table } from 'primeng/table';
 
 @Component({
     selector: 'top-details',
@@ -9,7 +9,7 @@ import {Table} from 'primeng/table';
 export class TopDetailsComponent implements AfterViewInit {
 
     riskUsersList;
-    threatsList;
+    threatsList: any[];
     violationsList;
 
     @Input() componentType: string;
@@ -33,18 +33,20 @@ export class TopDetailsComponent implements AfterViewInit {
     ];
 
     assignee = [{ name: 'User', value: 'user' }, { name: 'IP Address', value: 'ip' }, { name: 'Host', value: 'host' }];
-    
+
     selectedRiskies: any[];
     privilegedUsers: any[];
     dormantUsers: any[];
     serviceAccount: any[];
     watchlistedUsers: any[];
-    
+
     @ViewChild('selectedRiskyType') riskyTypeTable: Table;
     riskyTypeSelected = 'user';
 
     constructor(private topDetailsService: TopDetailsService) {
         this.selectedRiskies = this.riskyObjects.filter(riskyObj => riskyObj.type == 'user');
+
+        this.riskyObjects.sort((a, b) => a.score - b.score); // desending order
     }
 
     SelectedRiskyType($event) {
@@ -55,25 +57,25 @@ export class TopDetailsComponent implements AfterViewInit {
 
     ngAfterViewInit() {
         switch (this.componentType) {
-            case 'riskyUser' :
+            case 'riskyUser':
                 this.getRiskyUser();
                 break;
-            case 'topThreats' :
+            case 'topThreats':
                 this.getThreats();
                 break;
-            case 'topViolations' :
+            case 'topViolations':
                 this.getViolations();
                 break;
-            case 'privilegedUsers' :
+            case 'privilegedUsers':
                 this.getPrivilegedUsers();
                 break;
-            case 'dormantUsers' :
+            case 'dormantUsers':
                 this.getDormantUsers();
                 break;
-            case 'serviceAccount' :
+            case 'serviceAccount':
                 this.getServiceAccounts();
                 break;
-            case 'watchlistedUsers' :
+            case 'watchlistedUsers':
                 this.getWatchlistedUsers();
                 break;
         }
@@ -86,45 +88,70 @@ export class TopDetailsComponent implements AfterViewInit {
                 return b.riskscore - a.riskscore;
             });
         }); */
-
         this.selectedRiskies = this.riskyObjects.filter(risky => risky.type == 'user');
     }
 
     getPrivilegedUsers() {
         this.privilegedUsers = this.riskyObjects.filter(risky => risky.type == 'user');
+        this.privilegedUsers.sort((a, b) => a.score - b.score);
     }
 
     getDormantUsers() {
         this.dormantUsers = this.riskyObjects.filter(risky => risky.type == 'user');
+        this.dormantUsers.sort((a, b) => a.score - b.score);
     }
 
     getServiceAccounts() {
         this.serviceAccount = this.riskyObjects.filter(risky => risky.type == 'user');
+        this.serviceAccount.sort((a, b) => a.score - b.score);
     }
 
     getWatchlistedUsers() {
         this.watchlistedUsers = this.riskyObjects.filter(risky => risky.type == 'user');
+        this.watchlistedUsers.sort((a, b) => a.score - b.score);
     }
 
-
-    
     getThreats() {
-        this.topDetailsService.getTopThreats().subscribe((res: any) => {
-            /*for (const threat of res.data) {
+        /* this.topDetailsService.getTopThreats().subscribe((res: any) => {
+            for (const threat of res.data) {
                 const genDate = threat.displaytimestamp.generatedTimestampISODate;
                 threat.daysFromNow = moment(genDate,'YYYYMMDD').fromNow();
                 threat.days = moment(genDate).format('ddd, DD MMM YY');
                 threat.time = moment(genDate).format('hh:mm:ss');
-            }*/
-            this.threatsList = res.data;
-        });
+            }
+            this.threatsList = res.data;  - dynamic value from db
+        }); */
+
+        this.threatsList = [
+            {
+                title: 'Multiple users logged-in  successfully from same IP',
+                count: 47
+            },
+            {
+                title: 'Same IP is used for failed login attempts by 4 or more accounts',
+                count: 93
+            },
+            {
+                title: 'Interactive login attempt by user without badge activity',
+                count: 18
+            },
+            {
+                title: 'Failed interactive login attempts by users without proper username',
+                count: 3
+            },
+            {
+                title: 'Same user tried to login from 4 or more IP addresses',
+                count: 11
+            }
+        ];
+
+        this.threatsList.sort((a, b) => b.count - a.count);
     }
+
     getViolations() {
         this.topDetailsService.getTopViolations().subscribe((res: any) => {
             this.violationsList = res.data;
-            this.violationsList.sort((a, b) => {
-                return b.count - a.count;
-            });
+            this.violationsList.sort((a, b) => b.count - a.count);
         });
     }
 }

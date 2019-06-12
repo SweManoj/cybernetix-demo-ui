72d6863ab8a1d4404dfc8f1462c75e34a4f3a61d
unknown <manoj.kumar@nmbrsystems.com>
2019-06-12 20:17:57 +0530

    top blocks changes

--- src/app/pages/dashboard/components/topDetails/topDetails.component.ts
+++ src/app/pages/dashboard/components/topDetails/topDetails.component.ts
@@ -30,72 +30,72 @@ export class TopDetailsComponent implements AfterViewInit {
         { type: 'host', value: 'CHNLAP963', score: 65, img: false },
         { type: 'host', value: 'LONDESK588', score: 49, img: false },
         { type: 'host', value: 'AUSLAP4873', score: 30, img: false }
-    ];    
-      
+    ];
+
     terminatedUsersObjects = [
-        { type: 'user', value: 'Marci Belfield', score: 119, img: true },
-        { type: 'user', value: 'Christie Mays', score: 91, img: true },
-        { type: 'user', value: 'Shaquita', score: 79, img: true },
-        { type: 'user', value: 'Tandy', score: 62, img: true },
-        { type: 'user', value: 'Oralee Tena', score: 59, img: true }
+        { score: 119, image: 'Yamasaki' },
+        { score: 91, image: 'Shaquita' },
+        { score: 79, image: 'Tandy' },
+        { score: 62, image: 'Alysa' },
+        { score: 59, image: 'Beth Gee' }
     ];
 
     orphanUsersObjects = [
-        { type: 'user', value: 'Brunilda', score: 97, img: true },
-        { type: 'user', value: 'Juliann Chadwell', score: 91, img: true },
-        { type: 'user', value: 'Adella Morrow', score: 85, img: true },
-        { type: 'user', value: 'Beth Gee', score: 76, img: true },
-        { type: 'user', value: 'Drusilla', score: 63, img: true }
+        { score: 97, image: 'Brunilda' },
+        { score: 91, image: 'Adella Morrow' },
+        { score: 85, image: 'Drusilla' },
+        { score: 76, image: 'Stella' },
+        { score: 63, image: 'Juliann Chadwell' }
     ];
-  
+
     externalUsersObjects = [
-        { type: 'user', value: 'Tomika Napoleon', score: 133, img: true },
-        { type: 'user', value: 'Stella', score: 95, img: true },
-        { type: 'user', value: 'Glendora', score: 86, img: true },
-        { type: 'user', value: 'Antonietta', score: 75, img: true },
-        { type: 'user', value: 'Coral', score: 69, img: true }
+        { score: 133, image: 'Coral' },
+        { score: 95, image: 'Tina' },
+        { score: 86, image: 'Maile' },
+        { score: 75, image: 'Sarah Carmichael' },
+        { score: 69, image: 'Kylie Mier' }
     ];
 
     riskyCloudUsersObjects = [
-        { type: 'user', value: 'Tina ', score: 131, img: true },
-        { type: 'user', value: 'Roger Tilton', score: 94, img: true },
-        { type: 'user', value: 'Lorenza Hurrell', score: 83, img: true },
-        { type: 'user', value: 'Sarah Carmichael', score: 77, img: true },
-        { type: 'user', value: 'Maile', score: 72, img: true }
+        { score: 131, image: 'Heidy' },
+        { score: 91, image: 'Sondra Hildebrand' },
+        { score: 69, image: 'Kylie Mier' },
+        { score: 79, image: 'Maragret' },
+        { score: 45, image: 'Hazel' }
     ];
 
     privilegedUsersObjects = [
-        { type: 'user', value: 'Dannette Gratton', score: 104, img: true },
-        { type: 'user', value: 'Kylie Mier', score: 71, img: true },
-        { type: 'user', value: 'Sondra Hildebrand', score: 62, img: true },
-        { type: 'user', value: 'Darrin Kerrigan', score: 57, img: true },
-        { type: 'user', value: 'Heidy', score: 52, img: true }
+        { score: 104, image: 'Maile' },
+        { score: 71, image: 'Stella' },
+        { score: 62, image: 'Shayla Simo' },
+        { score: 57, image: 'Tina' },
+        { score: 49, image: 'Coral' }
     ];
 
     dormantUsersObjects = [
-        { type: 'user', value: 'Chrystal Marois', score: 124, img: true },
-        { type: 'user', value: 'Shayla Simo', score: 95, img: true },
-        { type: 'user', value: 'Adella Morrow', score: 84, img: true },
-        { type: 'user', value: 'Maragret', score: 67, img: true },
-        { type: 'user', value: 'Hazel', score: 61, img: true }
+        { score: 124, image: 'Charlotte' },
+        { score: 96, image: 'Mendelson' },
+        { score: 84, image: 'Kazuko' },
+        { score: 67, image: 'Nita' },
+        { score: 51, image: 'Nada Scheerer' }
     ];
-    
+
     serviceAccountObjects = [
-        { type: 'user', value: 'Chieko Montesino', score: 112, img: true },
-        { type: 'user', value: 'Charlotte', score: 98, img: true },
-        { type: 'user', value: 'Adella Morrow', score: 76, img: true },
-        { type: 'user', value: 'Mendelson', score: 65, img: true },
-        { type: 'user', value: 'Lura Feiler', score: 59, img: true }
+        { score: 112, image: 'Drusilla' },
+        { score: 98, image: 'Juliann Chadwell' },
+        { score: 76, image: 'Kylie Mier' },
+        { score: 65, image: 'Coral' },
+        { score: 59, image: 'Tina' }
     ];
 
     watchlistedUsersObjects = [
-        { type: 'user', value: 'Kazuko ', score: 137, img: true },
-        { type: 'user', value: 'Yamasaki', score: 107, img: true },
-        { type: 'user', value: 'Nada Scheerer', score: 93, img: true },
-        { type: 'user', value: 'Alysa', score: 72, img: true },
-        { type: 'user', value: 'Nita', score: 68, img: true }
+        { score: 137, image: 'Drusilla' },
+        { score: 107, image: 'Alysa' },
+        { score: 93, image: 'Brunilda' },
+        { score: 72, image: 'Yamasaki' },
+        { score: 68, image: 'Stella' }
     ];
-  
+
     assignee = [{ name: 'User', value: 'user' }, { name: 'IP Address', value: 'ip' }, { name: 'Host', value: 'host' }];
 
     selectedRiskies: any[];
@@ -114,9 +114,7 @@ export class TopDetailsComponent implements AfterViewInit {
     constructor(private topDetailsService: TopDetailsService) {
         this.selectedRiskies = this.riskyObjects.filter(riskyObj => riskyObj.type == 'user');
 
-        this.riskyObjects.sort((a, b) => -(a.score - b.score)); // desending order
-
-        
+        // this.riskyObjects.sort((a, b) => -(a.score - b.score)); -- desending order
     }
 
     SelectedRiskyType($event) {
@@ -174,42 +172,42 @@ export class TopDetailsComponent implements AfterViewInit {
     }
 
     getPrivilegedUsers() {
-        this.privilegedUsers = this.privilegedUsersObjects.filter(risky => risky.type == 'user');
+        this.privilegedUsers = this.privilegedUsersObjects;
         this.privilegedUsers.sort((a, b) => -(a.score - b.score));
     }
 
     getTerminatedUsers() {
-        this.terminatedUsers = this.terminatedUsersObjects.filter(risky => risky.type == 'user');
+        this.terminatedUsers = this.terminatedUsersObjects;
         this.terminatedUsers.sort((a, b) => -(a.score - b.score));
     }
 
     getOrphanUsers() {
-        this.orphanUsers = this.orphanUsersObjects.filter(risky => risky.type == 'user');
+        this.orphanUsers = this.orphanUsersObjects;
         this.orphanUsers.sort((a, b) => -(a.score - b.score));
     }
 
     getExternalUsers() {
-        this.externalUsers = this.externalUsersObjects.filter(risky => risky.type == 'user');
+        this.externalUsers = this.externalUsersObjects;
         this.externalUsers.sort((a, b) => -(a.score - b.score));
     }
 
     getRiskyCloudUsers() {
-        this.riskyCloudUsers = this.riskyCloudUsersObjects.filter(risky => risky.type == 'user');
+        this.riskyCloudUsers = this.riskyCloudUsersObjects;
         this.riskyCloudUsers.sort((a, b) => -(a.score - b.score));
     }
 
     getDormantUsers() {
-        this.dormantUsers = this.dormantUsersObjects.filter(risky => risky.type == 'user');
+        this.dormantUsers = this.dormantUsersObjects;
         this.dormantUsers.sort((a, b) => -(a.score - b.score));
     }
 
     getServiceAccounts() {
-        this.serviceAccount = this.serviceAccountObjects.filter(risky => risky.type == 'user');
+        this.serviceAccount = this.serviceAccountObjects;
         this.serviceAccount.sort((a, b) => -(a.score - b.score));
     }
 
     getWatchlistedUsers() {
-        this.watchlistedUsers = this.watchlistedUsersObjects.filter(risky => risky.type == 'user');
+        this.watchlistedUsers = this.watchlistedUsersObjects;
         this.watchlistedUsers.sort((a, b) => -(a.score - b.score));
     }
 

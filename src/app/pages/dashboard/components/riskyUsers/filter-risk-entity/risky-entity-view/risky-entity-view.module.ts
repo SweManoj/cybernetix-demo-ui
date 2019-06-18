77abac53216631f8b7af8a6c37c914be1dd1e836
unknown <manoj.kumar@nmbrsystems.com>
2019-06-18 22:38:31 +0530

    filter risky page changes

--- src/app/pages/dashboard/components/riskyUsers/filter-risk-entity/risky-entity-view/risky-entity-view.module.ts
+++ src/app/pages/dashboard/components/riskyUsers/filter-risk-entity/risky-entity-view/risky-entity-view.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { RiskyEntityViewComponent } from './risky-entity-view';
 import { MaterialModule } from './../../../../../../material';
 

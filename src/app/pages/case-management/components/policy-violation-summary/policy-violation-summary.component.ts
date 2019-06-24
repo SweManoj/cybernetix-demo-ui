dbca8a75a900dba8137384ea77dda988fc6ee26c
1986webdeveloper <1986webdeveloper@gmail.com>
2019-06-24 13:59:34 +0530

    Solve Dashboard UI confilct

--- src/app/pages/case-management/components/policy-violation-summary/policy-violation-summary.component.ts
+++ src/app/pages/case-management/components/policy-violation-summary/policy-violation-summary.component.ts
@@ -33,8 +33,26 @@ export class PolicyViolationSummaryComponent implements OnInit {
         userId: "chetan@123",
         content: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown" +
             " printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-        timestamp: this.d.setHours(this.d.getHours() - 4),
+        timestamp: this.d.setHours(this.d.getHours() - 3),
         commentId: 3,
+        parentId: 2,
+        reply: false
+    },
+    {
+        userId: "chetan@123",
+        content: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown" +
+            " printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+        timestamp: this.d.setHours(this.d.getHours() - 1),
+        commentId: 4,
+        parentId: 2,
+        reply: false
+    },
+    {
+        userId: "chetan@123",
+        content: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown" +
+            " printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+        timestamp: this.d.setHours(this.d.getHours() - 5),
+        commentId: 5,
         parentId: 2,
         reply: false
     }];

0c20b6aa714f60ac5016708a0a57224d11df9c17
Disha-niranjan <disha.niranjan@nmbrsystems.com>
2019-06-26 18:11:26 +0530

    Added Linechart for Risky Host Timeline

--- src/app/pages/dashboard/components/riskyHost/riskyHost.component.ts
+++ src/app/pages/dashboard/components/riskyHost/riskyHost.component.ts
@@ -4,6 +4,9 @@ import { RiskyUserService } from '../riskyUsers/riskyUser.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RiskScoreModalComponent } from '../riskyUsers/risk-score-modal/risk-score-modal.component';
+import * as am4core from '@amcharts/amcharts4/core';
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import * as am4charts from '@amcharts/amcharts4/charts';
 
 @Component({
     selector: 'app-riskyHost',
@@ -120,11 +123,74 @@ export class RiskyHostComponent implements OnInit {
         });
     }
 
-   /*  ngAfterViewInit() {
+    initializeLineChart() {
+
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        let chart = am4core.create("lineChartDiv", am4charts.XYChart);
+        chart.paddingRight = 20;
+
+        let data = [];
+        let visits = 10;
+        let previousValue;
+
+        for (var i = 0; i < 100; i++) {
+            visits = Math.round(i * Math.random());
+
+            if (visits > 100) {
+                visits = 80 + i;
+            }
+
+            if (i > 0) {
+                // add color to previous data item depending on whether current value is less or more than previous value
+                if (previousValue <= 65)
+                    data[i - 1].color = am4core.color('#ADFF2F');
+                else if (previousValue > 65 && previousValue <= 79)
+                    data[i - 1].color = am4core.color('#FFA500');
+                else
+                    data[i - 1].color = am4core.color('#f00');
+
+            }
+
+            data.push({ date: new Date(2018, 0, i + 1), value: visits });
+            previousValue = visits;
+        }
+
+        chart.data = data;
+
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+        dateAxis.renderer.axisFills.template.disabled = true;
+        dateAxis.renderer.ticks.template.disabled = true;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
+        valueAxis.renderer.axisFills.template.disabled = true;
+        valueAxis.renderer.ticks.template.disabled = true;
+
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "date";
+        series.dataFields.valueY = "value";
+        series.strokeWidth = 2;
+        series.tooltipText = "value: {valueY}, date: {dateX}";
+
+        // set stroke property field
+        series.propertyFields.stroke = "color";
+
+        chart.cursor = new am4charts.XYCursor();
+
+        let scrollbarX = new am4core.Scrollbar();
+        chart.scrollbarX = scrollbarX;
+
+    }
+
+    ngAfterViewInit() {
         this.zone.runOutsideAngular(() => {
             this.initializeLineChart();
         });
-    } */
+    } 
 
     ngOnInit() {
         this.routeParam.paramMap.subscribe((params) => {

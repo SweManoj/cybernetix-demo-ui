2a66980c6cec5bebc982dc056fd5894cb4dad247
unknown <manoj.kumar@nmbrsystems.com>
2019-06-26 17:03:03 +0530

    remove unused chart from components

--- src/app/pages/dashboard/components/riskyHost/riskyHost.component.ts
+++ src/app/pages/dashboard/components/riskyHost/riskyHost.component.ts
@@ -1,13 +1,9 @@
-import {Component, NgZone, OnInit,AfterContentInit} from '@angular/core';
-import {AmChartsService} from '@amcharts/amcharts3-angular';
-import {RiskyUserService} from '../riskyUsers/riskyUser.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import * as am4core from '@amcharts/amcharts4/core';
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import * as am4charts from '@amcharts/amcharts4/charts';
-import {bubbleDataMonth} from '../riskyUsers/data';
-import {RiskScoreModalComponent} from '../riskyUsers/risk-score-modal/risk-score-modal.component';
+import { Component, NgZone, OnInit, AfterContentInit } from '@angular/core';
+import { AmChartsService } from '@amcharts/amcharts3-angular';
+import { RiskyUserService } from '../riskyUsers/riskyUser.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RiskScoreModalComponent } from '../riskyUsers/risk-score-modal/risk-score-modal.component';
 
 @Component({
     selector: 'app-riskyHost',
@@ -20,11 +16,11 @@ export class RiskyHostComponent implements OnInit {
     hostDetails: any;
 
     hostAddressData = [
-        { type: 'host', value: 'PUNDESK001', score: 30, img: false, location : 'Munich, Germany', lastSeen : '23 Jun 2019 03:22:00', peer : 2, lastSeenUser : 'PAV58329'},
-        { type: 'host', value: 'USADESK25', score: 89, img: false, location : 'Amsterdam, Netherlands ', lastSeen: '21 Jun 2019 17:10:00', peer : 3, lastSeenUser : 'NEI89321'},
-        { type: 'host', value: 'CHNLAP963', score: 66, img: false, location : 'Banglore, India', lastSeen: '22 Jun 2018 09:17:00', peer : 1, lastSeenUser : 'CAI67248'},
-        { type: 'host', value: 'LONDESK588', score: 49, img: false, location : 'Beijing, China', lastSeen: '23 Jun 2019 13:09:00', peer : 2, lastSeenUser : 'SAU76518'},
-        { type: 'host', value: 'AUSLAP4873', score: 70, img: false, location : 'Paris, France', lastSeen : '24 Jun 2019 18:38:00', peer : 3, lastSeenUser : 'JRU87122'}];
+        { type: 'host', value: 'PUNDESK001', score: 30, img: false, location: 'Munich, Germany', lastSeen: '23 Jun 2019 03:22:00', peer: 2, lastSeenUser: 'PAV58329' },
+        { type: 'host', value: 'USADESK25', score: 89, img: false, location: 'Amsterdam, Netherlands ', lastSeen: '21 Jun 2019 17:10:00', peer: 3, lastSeenUser: 'NEI89321' },
+        { type: 'host', value: 'CHNLAP963', score: 66, img: false, location: 'Banglore, India', lastSeen: '22 Jun 2018 09:17:00', peer: 1, lastSeenUser: 'CAI67248' },
+        { type: 'host', value: 'LONDESK588', score: 49, img: false, location: 'Beijing, China', lastSeen: '23 Jun 2019 13:09:00', peer: 2, lastSeenUser: 'SAU76518' },
+        { type: 'host', value: 'AUSLAP4873', score: 70, img: false, location: 'Paris, France', lastSeen: '24 Jun 2019 18:38:00', peer: 3, lastSeenUser: 'JRU87122' }];
 
     hardCodeItemData = [
         {
@@ -67,7 +63,7 @@ export class RiskyHostComponent implements OnInit {
                 title: 'Suspicious Data Objects Downloaded By Service Account - Fileshare'
             }
         },
-       
+
     ];
 
     threatCategories = [
@@ -112,37 +108,30 @@ export class RiskyHostComponent implements OnInit {
         } */
     ];
 
-
     constructor(private amChartService: AmChartsService, private riskyUserService: RiskyUserService, private routeParam: ActivatedRoute, private modalService: NgbModal,
-                private zone: NgZone, private router: Router) {
+        private zone: NgZone, private router: Router) {
     }
 
-    getDataByHost(){
+    getDataByHost() {
         this.hostAddressData.forEach(hostData => {
-            if (hostData.value === this.selectedHost){
+            if (hostData.value === this.selectedHost) {
                 this.hostDetails = hostData;
             }
         });
     }
 
-    ngAfterViewInit() {
+   /*  ngAfterViewInit() {
         this.zone.runOutsideAngular(() => {
-            // Initialize Guage meter chart
-            this.initializeGuageMeterChart();
-
-            // Initialize Bubble chart
             this.initializeLineChart();
         });
-    }
+    } */
 
     ngOnInit() {
-
         this.routeParam.paramMap.subscribe((params) => {
             this.selectedHost = params.get('selectedHost');
         });
 
         this.getDataByHost();
-
     }
 
     getRiskScoreColor(riskScore: number) {
@@ -152,141 +141,6 @@ export class RiskyHostComponent implements OnInit {
             return "darkorange";
         else
             return "crimson";
-    }
-
-    initializeGuageMeterChart() {
-
-        am4core.useTheme(am4themes_animated);
-        // create chart
-        var chart = am4core.create("chartGuageDiv", am4charts.GaugeChart);
-        chart.hiddenState.properties.opacity = 0; // this makes initial fade in effect
-
-        chart.innerRadius = -20;
-
-        // Set cell size in pixels
-        let cellSize = 30;
-        chart.events.on("datavalidated", function (ev) {
-
-            let chart = ev.target;
-
-        });
-
-
-        var axis = chart.xAxes.push(new am4charts.ValueAxis<am4charts.AxisRendererCircular>());
-        axis.min = 0;
-        axis.max = 100;
-        axis.strictMinMax = true;
-        axis.renderer.grid.template.stroke = new am4core.InterfaceColorSet().getFor("background");
-        axis.renderer.grid.template.strokeOpacity = 0.3;
-
-        // axis.fontSize = 0;
-        /* axis.hidden = false;
-        axis.fixedWidthGrid = true; */
-
-        // axis.renderer.ticks.template.length = 35;
-        // axis.renderer.grid.template.disabled = true;
-        axis.fontSize = 12;
-        axis.renderer.labels.template.radius = 4;   // space b/w meter and number
-        /* axis.renderer.labels.template.adapter.add("text", function (text) {
-            return text + "%";
-        }) */
-
-        var range0 = axis.axisRanges.create();
-        range0.value = 0;
-        range0.endValue = 65;
-        range0.axisFill.fillOpacity = 1;
-        range0.axisFill.fill = am4core.color('greenyellow')
-        range0.axisFill.zIndex = - 1;
-
-        var range1 = axis.axisRanges.create();
-        range1.value = 65;
-        range1.endValue = 79;
-        range1.axisFill.fillOpacity = 1;
-        range1.axisFill.fill = am4core.color('darkorange')
-        range1.axisFill.zIndex = -1;
-
-        var range2 = axis.axisRanges.create();
-        range2.value = 79;
-        range2.endValue = 100;
-        range2.axisFill.fillOpacity = 1;
-        range2.axisFill.fill = am4core.color('crimson')
-        range2.axisFill.zIndex = -1;
-
-        var hand = chart.hands.push(new am4charts.ClockHand());
-        hand.value = this.hostDetails.score;
-        hand.fill = am4core.color("#2D93AD");   // hand color
-        hand.stroke = am4core.color("#2D93AD");
-
-        // using chart.setTimeout method as the timeout will be disposed together with a chart
-        chart.setTimeout(randomValue, 2000);
-
-        function randomValue() {
-            hand.showValue(this.hostDetails.score, 1000, am4core.ease.cubicOut);
-            chart.setTimeout(randomValue, 2000);
-        }
-    }
-
-    initializeLineChart() {
-
-        am4core.useTheme(am4themes_animated);
-// Themes end
-
-        let chart = am4core.create("lineChartDiv", am4charts.XYChart);
-        chart.paddingRight = 20;
-
-        let data = [];
-        let visits = 10;
-        let previousValue;
-
-        for (var i = 0; i < 100; i++) {
-            visits = Math.round(i * Math.random());
-
-            if(visits > 100){
-                visits = 80 + i;
-            }
-
-            if(i > 0){
-                // add color to previous data item depending on whether current value is less or more than previous value
-                if(previousValue <= 65)
-                    data[i - 1].color = am4core.color('#ADFF2F');
-                else if (previousValue > 65 && previousValue <= 79)
-                    data[i - 1].color = am4core.color('#FFA500');
-                else
-                    data[i - 1].color = am4core.color('#f00');
-
-            }
-
-            data.push({ date: new Date(2018, 0, i + 1), value: visits });
-            previousValue = visits;
-        }
-
-        chart.data = data;
-
-        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-        dateAxis.renderer.grid.template.location = 0;
-        dateAxis.renderer.axisFills.template.disabled = true;
-        dateAxis.renderer.ticks.template.disabled = true;
-
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-        valueAxis.tooltip.disabled = true;
-        valueAxis.renderer.minWidth = 35;
-        valueAxis.renderer.axisFills.template.disabled = true;
-        valueAxis.renderer.ticks.template.disabled = true;
-
-        let series = chart.series.push(new am4charts.LineSeries());
-        series.dataFields.dateX = "date";
-        series.dataFields.valueY = "value";
-        series.strokeWidth = 2;
-        series.tooltipText = "value: {valueY}, date: {dateX}";
-
-// set stroke property field
-        series.propertyFields.stroke = "color";
-
-        chart.cursor = new am4charts.XYCursor();
-
-        let scrollbarX = new am4core.Scrollbar();
-        chart.scrollbarX = scrollbarX;
-
     }
 
     open(ruilId, userId, isotimestamp) {

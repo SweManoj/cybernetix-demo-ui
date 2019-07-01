fee4ec29f2943df61271b1b3d0a37abb63a639f2
Disha-niranjan <disha.niranjan@nmbrsystems.com>
2019-07-01 17:13:31 +0530

    charts styles

--- src/app/pages/dashboard/components/globe-chart/globe-chart-component.ts
+++ src/app/pages/dashboard/components/globe-chart/globe-chart-component.ts
@@ -28,8 +28,10 @@ export class GlobeChartComponent implements AfterViewInit {
         },
         title: {
             align: 'left',
-            style : { color : '#a0a0a0', fontWeight : 'bold'},
-            text: 'RISK BY DEPARTMENT'
+            style : { color : '#a0a0a0', fontWeight : 'bold', fontFamily : 'roboto' },
+            text: 'RISK BY DEPARTMENT',
+            x: 8,
+            y: 24
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.percentage:.0f}</b>'
@@ -82,15 +84,19 @@ export class GlobeChartComponent implements AfterViewInit {
           },
           title: {
             align: 'left',
-            style : { color : '#a0a0a0', fontWeight : 'bold'},
-            text: 'RISK BY TITLE'
+            style : { color : '#a0a0a0', fontWeight : 'bold', fontFamily : 'roboto' },
+            text: 'RISK BY TITLE',
+            x: 8,
+            y: 24
           },
           xAxis: {
             categories: [
               'VP-Sales', 'Software Engineer', 'Sr. Tester', 'Project Manager', 'Product Owner'
             ],
             labels : {
-              style : { color : '#fff'},
+                step: 1,
+                width : "20px",
+              style : { color : '#fff'},            
             }
           },
           yAxis: {
@@ -143,96 +149,96 @@ export class GlobeChartComponent implements AfterViewInit {
 
     constructor(private zone: NgZone) { }
 
-    public pieChartOptions: ChartOptions = {
-        responsive: true,
-        legend: {
-            position: 'right',
-            labels: {
-                fontFamily: 'Calibri',
-                fontSize: 12,
-                fontColor: '#a0a0a0',
-                fontStyle: 'bold',
-                padding: 24,
-            },
+    // public pieChartOptions: ChartOptions = {
+    //     responsive: true,
+    //     legend: {
+    //         position: 'right',
+    //         labels: {
+    //             fontFamily: 'Calibri',
+    //             fontSize: 12,
+    //             fontColor: '#a0a0a0',
+    //             fontStyle: 'bold',
+    //             padding: 24,
+    //         },
 
-        },
-        plugins: {
-            datalabels: {
-                formatter: (value, ctx) => {
-                    const label = ctx.chart.data.labels[ctx.dataIndex];
-                    return label;
-                },
-            },
-        }
-    };
-    public pieChartLabels: Label[] = ['IT Security', 'Infrastructure', 'IT Support', 'Quality Testing', 'Development'];
-    public pieChartData: number[] = [28, 15, 10, 12, 30];
-    public pieChartType: ChartType = 'pie';
-    public pieChartLegend = true;
-    public pieChartColors = [
-        {
-            backgroundColor: ['#581845', '#ffc305', '#c70039', '#ff5733', '#900c3f'],
-        },
-    ];
+    //     },
+    //     plugins: {
+    //         datalabels: {
+    //             formatter: (value, ctx) => {
+    //                 const label = ctx.chart.data.labels[ctx.dataIndex];
+    //                 return label;
+    //             },
+    //         },
+    //     }
+    // };
+    // public pieChartLabels: Label[] = ['IT Security', 'Infrastructure', 'IT Support', 'Quality Testing', 'Development'];
+    // public pieChartData: number[] = [28, 15, 10, 12, 30];
+    // public pieChartType: ChartType = 'pie';
+    // public pieChartLegend = true;
+    // public pieChartColors = [
+    //     {
+    //         backgroundColor: ['#581845', '#ffc305', '#c70039', '#ff5733', '#900c3f'],
+    //     },
+    // ];
 
-    public barChartOptions: ChartOptions = {
-        responsive: true,
-        scales: {
-            yAxes: [
-                {
-                    display: true,
-                    ticks: {
-                        beginAtZero: true,
-                        fontFamily: 'Calibri',
-                        fontSize: 10,
-                        fontColor: '#a0a0a0',
-                        //fontStyle : 'bold'
-                    },
+    // public barChartOptions: ChartOptions = {
+    //     responsive: true,
+    //     scales: {
+    //         yAxes: [
+    //             {
+    //                 display: true,
+    //                 ticks: {
+    //                     beginAtZero: true,
+    //                     fontFamily: 'Calibri',
+    //                     fontSize: 10,
+    //                     fontColor: '#a0a0a0',              
+    //                     //fontStyle : 'bold'
+    //                 },
 
-                }
+    //             }
 
-            ],
-            xAxes: [{
-                gridLines: {
-                    offsetGridLines: true
-                },
-                ticks: {
-                    fontFamily: 'Calibri',
-                    fontSize: 10,
-                    fontColor: '#a0a0a0',
-                    //fontStyle : 'bold',
-                    maxRotation: 0
-                },
-            }]
-        },
-        legend: {
-            display: false,
-            labels: {
-                fontFamily: 'Calibri',
-                fontSize: 10,
-                fontColor: '#a0a0a0',
-                fontStyle: 'bold',
-            },
-        }
-    };
+    //         ],
+    //         xAxes: [{
+    //             gridLines: {
+    //                 offsetGridLines: true
+    //             },
+    //             ticks: {
+    //                 fontFamily: 'Calibri',
+    //                 fontSize: 10,
+    //                 fontColor: '#a0a0a0',
+    //                 //fontStyle : 'bold',
+    //                 maxRotation: 0
+    //             },
+    //         }]
+    //     },
+    //     legend: {
+    //         display: false,
+    //         labels: {
+    //             fontFamily: 'Calibri',
+    //             fontSize: 10,
+    //             fontColor: '#a0a0a0',
+    //             fontStyle: 'bold',
+    //         },
+    //     }
+    // };
 
-    public barChartLabels: Label[] = ['VP-Sales', 'Sr. Software Engineer', 'Sr. Tester', 'Project Manager', 'Product Owner'];
-    public barChartType: ChartType = 'bar';
-    public barChartLegend = false;
-    public barChartPlugins = [];
+    // public barChartLabels: Label[] = ['VP-Sales', 'Sr. Software Engineer', 'Sr. Tester', 'Project Manager', 'Product Owner'];
+    // public barChartType: ChartType = 'bar';
+    // public barChartLegend = false;
+    // public barChartPlugins = [];
 
 
-    public barChartData: ChartDataSets[] = [
-        {
-            data: [65, 59, 80, 81, 56, 55, 40, 30, 10, 100], label: 'Risks by Titles', backgroundColor: [
-                "cadetblue", "deepskyblue", "palegreen", "darkcyan", "lightcoral"
-            ],
-            borderColor: [
-                "#111", "#111", "#111", "#111", "#111"
-            ],
-            borderWidth: 1
-        }
-    ];
+    // public barChartData: ChartDataSets[] = [
+    //     {
+    //         data: [65, 59, 80, 81, 56, 55, 40, 30, 10, 100], label: 'Risks by Titles', backgroundColor: [
+    //             "cadetblue", "deepskyblue", "palegreen", "darkcyan", "lightcoral"
+    //         ],
+    //         borderColor: [
+    //             "#111", "#111", "#111", "#111", "#111"
+    //         ],
+    //         borderWidth: 1
+    //     }
+    // ];
 
     ngAfterViewInit() {
         Highcharts.chart('container', this.options);

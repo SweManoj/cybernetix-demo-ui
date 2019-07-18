d57b8774011ca3f59d716444a4230dc0ac070366
Disha-niranjan <disha.niranjan@nmbrsystems.com>
2019-07-18 18:21:38 +0530

    login url

--- src/app/core/login/login.service.ts
+++ src/app/core/login/login.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { RequestOptions, Headers, } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-
-@Injectable()
+import { config } from '../../../config';
+import { Tokens } from '../../auth/models/token';
+import { tap } from 'rxjs/internal/operators/tap';
+import { environment } from '../../../environments/environment'
+@Injectable({
+  providedIn: 'root'
+})
 export class LoginService {
-    loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  private loggedUser: string;
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    login(loginData) : Observable<any> {
-        return this.http.post(`http://localhost:9090/oauth/token?grant_type=password&username=${loginData.username}&password=${loginData.password}`,null,{headers:{"Authorization": "Basic Y3liZXJuZXRpeC1jbGllbnQ6c2VjcmV0"}});
-    }
+  login(loginData): Observable<any> {
+    return this.http.post(`http://localhost:9090/cybernetix/oauth/token?grant_type=password&username=${loginData.username}&password=${loginData.password}`, null, { headers: { "Authorization": "Basic Y3liZXJuZXRpeC1jbGllbnQ6c2VjcmV0" } });
+  }
 
-    get isLoggedIn() {
-        return this.loggedIn.asObservable();
-    }
+  get isLoggedIn() {
+    return this.loggedIn.asObservable();
+  }
+
+  refreshAuthToken(refreshtoken): Observable<any> {
+    return this.http.post(`http://localhost:9090/cybernetix/oauth/token?grant_type=refresh_token&refresh_token=${refreshtoken}`, null, { headers: { "Authorization": "Basic Y3liZXJuZXRpeC1jbGllbnQ6c2VjcmV0" } });
+  }
+
 }

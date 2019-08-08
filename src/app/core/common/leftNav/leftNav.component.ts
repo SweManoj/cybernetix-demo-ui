5bfc70f7ac2b5cb6ee8a92cdbea1dd609f4830f1
kshitija <kshitija.vdharwadkar@non.se.com>
2019-08-08 12:56:28 +0530

    UAT Bug fixes

--- src/app/core/common/leftNav/leftNav.component.ts
+++ src/app/core/common/leftNav/leftNav.component.ts
@@ -21,7 +21,7 @@ export class LeftNavComponent {
                 ionIcon: '',
                 faIcon: 'fa fa-lock',
                 img: '',
-                title: 'Policies', // this.translateService.instant('general.menu.dashboard')
+                title: 'Violations', // this.translateService.instant('general.menu.dashboard')
                 navLink: '/policyViolation'
             },
             {

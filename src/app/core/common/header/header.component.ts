161ed6ccf63276baf6679ecf4c66cce4d090ea12
kshitija <kshitija.vdharwadkar@non.se.com>
2019-08-30 14:05:21 +0530

    Added search for host and IP

--- src/app/core/common/header/header.component.ts
+++ src/app/core/common/header/header.component.ts
@@ -1,16 +1,16 @@
-import { Component, Inject } from '@angular/core';
-import { UserContext } from '../../services/userContext';
-import { Router } from '@angular/router';
-import { LoginService } from '../../login/login.service';
-import { UtilService } from '../../services/util.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
-import { UtilDataService } from '../../services/util.data.service';
-import { ModalUtilComponent } from '../modal-util/modal.util.component';
-import { DashboardService } from '../../../pages/dashboard/dashboard.service';
-import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
-import { StorageService, SESSION_STORAGE } from 'angular-webstorage-service';
+import {Component, Inject} from '@angular/core';
+import {UserContext} from '../../services/userContext';
+import {Router} from '@angular/router';
+import {LoginService} from '../../login/login.service';
+import {UtilService} from '../../services/util.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {UtilDataService} from '../../services/util.data.service';
+import {ModalUtilComponent} from '../modal-util/modal.util.component';
+import {DashboardService} from '../../../pages/dashboard/dashboard.service';
+import {DEFAULT_INTERRUPTSOURCES, Idle} from '@ng-idle/core';
+import {SESSION_STORAGE, StorageService} from 'angular-webstorage-service';
 
 @Component({
     selector: 'app-header',
@@ -26,16 +26,18 @@ export class HeaderComponent {
         direction: 'leftToRightIncreased',
         color: '#42b4e6',
         thick: true
-      };
+    };
     opened: boolean;
     themeName: string;
     private prevThemeName: string;
     riskyUsers = [];
+    riskyIPAddress = [];
+    riskyHosts = [];
 
     constructor(private userContext: UserContext, private router: Router,
-        idle: Idle, @Inject(SESSION_STORAGE) private sessionStorage: StorageService,
-        private loginService: LoginService, private utilService: UtilService, public modal: NgbModal,
-        private utilDataService: UtilDataService, private ngbModal: NgbModal, private dashboardService: DashboardService) {
+                idle: Idle, @Inject(SESSION_STORAGE) private sessionStorage: StorageService,
+                private loginService: LoginService, private utilService: UtilService, public modal: NgbModal,
+                private utilDataService: UtilDataService, private ngbModal: NgbModal, private dashboardService: DashboardService) {
 
         this.themeName = this.userContext.themeName;
         this.prevThemeName = this.themeName;
@@ -65,7 +67,8 @@ export class HeaderComponent {
         this.modal.open(settingsModal, {
             centered: true,
             size: 'lg'
-        }).result.then(() => { }, () => {
+        }).result.then(() => {
+        }, () => {
             this.updateTheme();
         });
     }
@@ -95,40 +98,100 @@ export class HeaderComponent {
             this.utilDataService.filteredRiskyUsers = [];
 
             this.dashboardService.searchUserByName(this.searchEntity).subscribe((res: any) => {
-                this.riskyUsers = res;
+                res = {
+                    "users": [
+                        {
+                            "u_userId": "8182cacf-915b-48b3-993d-2ecd02e466c7",
+                            "u_employeeId": "as24826",
+                            "u_firstName": "Andrea",
+                            "u_middleName": "NULL",
+                            "u_lastName": "Skeete",
+                            "u_title": "Merchandise Analyst",
+                            "u_departmentName": "Supply Chain",
+                            "u_startDate": "6/14/17 0:00",
+                            "u_phoneExtension": "NULL",
+                            "u_mobile": "NULL",
+                            "u_mailDrop": null,
+                            "u_address": "NULL",
+                            "u_buildingIndentifyer": null,
+                            "u_employeeType": "Employee",
+                            "u_email": "as24826@cybernetix.com",
+                            "u_departmentLevel2": null,
+                            "u_companyName": null,
+                            "u_country": "Indonesia",
+                            "u_immediateManagerLevel1": "dn24122",
+                            "u_managerReportingHiercy": null,
+                            "u_employeeStartDate": null,
+                            "u_employeeStatus": "acitve",
+                            "u_employeeTerminationDate": null,
+                            "u_lastLogonTimeStamp": null,
+                            "totalRiskScore": 0,
+                            "lastViolatedPolicyName": null,
+                            "lastViolatedViolationId": null,
+                            "lastViolatedEventDate": null,
+                            "lastgenerationtime": 0,
+                            "dormant": false
+                        }
+                    ],
+                    "ipAddresses": [
+                        {
+                            "ipValue": "10.10.3.10",
+                            "first_seen_at": "6/24/19 9:53",
+                            "last_seen_at": "6/24/19 9:53",
+                            "location": null,
+                            "peers": 0,
+                            "totalRiskScore": 0,
+                            "lastViolatedPolicyName": null,
+                            "lastViolatedViolationId": null,
+                            "lastViolatedEventDate": null,
+                            "lastgenerationtime": 0
+                        }
+                    ],
+                    "hosts": [
+                        {
+                            "hostName": "AZEUS2ADC01",
+                            "first_seen_at": "6/19/19 10:04",
+                            "last_seen_at": "6/19/19 10:04",
+                            "location": null,
+                            "peers": 0,
+                            "totalRiskScore": 0,
+                            "lastViolatedPolicyName": null,
+                            "lastViolatedViolationId": null,
+                            "lastViolatedEventDate": null,
+                            "lastgenerationtime": 0
+                        }
+                    ],
+                    "searchValue": "a"
+                };
+                this.riskyUsers = res.users;
+                this.riskyHosts = res.hosts;
+                this.riskyIPAddress = res.ipAddresses;
 
-                const forFilterDuplication = this.riskyUsers.filter(riskyUser =>
-                    riskyUser.u_firstName.toLowerCase().indexOf(this.searchEntity.toLowerCase()) !== -1)  // user name
-                    .concat(this.riskyUsers.filter(riskyUser =>
-                        riskyUser.u_departmentName.toLowerCase().indexOf(this.searchEntity.toLowerCase()) !== -1))  // department name
-                    .concat(this.riskyUsers.filter(riskyUser =>
-                        riskyUser.u_title.toLowerCase().indexOf(this.searchEntity.toLowerCase()) !== -1))  // role name
-                    .concat(this.riskyUsers.filter(riskyUser =>
-                        riskyUser.u_country.toLowerCase().indexOf(this.searchEntity.toLowerCase()) !== -1))  // location name
-
-                if (forFilterDuplication.length > 0) {
-                    this.utilDataService.filteredRiskyUsers = this.removeDuplicates(forFilterDuplication, 'u_employeeId');
+                if (this.riskyUsers.length > 0 || this.riskyHosts.length > 0 || this.riskyIPAddress.length > 0) {
+                    this.utilDataService.filteredRiskyUsers = this.riskyUsers;
+                    this.utilDataService.filteredRiskyHost = this.riskyHosts;
+                    this.utilDataService.filteredRiskyIPAddress = this.riskyIPAddress;
                     this.router.navigate(['/filteredRiskyUsers', this.searchEntity.toString()]);
-                } else {
-                    const modalRef = this.ngbModal.open(ModalUtilComponent, { size: 'sm', backdrop: 'static' }); // { size: 'sm' }
+                }
+
+
+
+                if (res.users.length === 0 && res.hosts.length === 0 && res.ipAddresses.length === 0) {
+                    const modalRef = this.ngbModal.open(ModalUtilComponent, {size: 'sm', backdrop: 'static'}); // { size: 'sm' }
                     modalRef.componentInstance.modalHeader = 'Warning';
                     modalRef.componentInstance.modalMessage = 'No Records Found !';
                     modalRef.componentInstance.cancelFlag = false;
+
                 }
+
             });
         } else {
-            const modalRef = this.ngbModal.open(ModalUtilComponent, { size: 'sm', backdrop: 'static' }); // { size: 'sm' }
+            const modalRef = this.ngbModal.open(ModalUtilComponent, {size: 'sm', backdrop: 'static'}); // { size: 'sm' }
             modalRef.componentInstance.modalHeader = 'Warning';
             modalRef.componentInstance.modalMessage = 'Please Enter Risky Entity Values !';
             modalRef.componentInstance.cancelFlag = false;
         }
     }
-
-    removeDuplicates(myArr: any[], prop: string) {
-        return myArr.filter((obj: any, pos: number, arr: any[]) => {
-            return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
-        });
-    };
 
     changeMenuOpen() {
         this.utilService.isMenuOpened = !this.utilService.isMenuOpened;
--- src/app/pages/dashboard/components/riskyUsers/filter-risk-entity/filter-risk-entity.component.ts
+++ src/app/pages/dashboard/components/riskyUsers/filter-risk-entity/filter-risk-entity.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { UtilDataService } from '../../../../../core/services/util.data.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {UtilDataService} from '../../../../../core/services/util.data.service';
 
 @Component({
-  selector: 'app-filter-risk-entity',
-  templateUrl: './filter-risk-entity.component.html',
-  styleUrls: ['./filter-risk-entity.component.scss']
+    selector: 'app-filter-risk-entity',
+    templateUrl: './filter-risk-entity.component.html',
+    styleUrls: ['./filter-risk-entity.component.scss']
 })
 export class FilterRiskEntityComponent implements OnInit {
 
-  foundEntityName = '';
-  filteredRiskyEntities: any[] = [];
+    foundEntityName = '';
+    filteredRiskyEntities: any[] = [];
+    filteredRiskyHosts: any[] = [];
+    filteredRiskyIPAddress: any[] = [];
 
-  constructor(private route: ActivatedRoute, private utilDataService: UtilDataService) {
+    constructor(private route: ActivatedRoute, private utilDataService: UtilDataService) {
 
-    this.route.params.subscribe((params: Params) => {
-      this.foundEntityName = params['riskyUser'];
+        this.route.params.subscribe((params: Params) => {
+            this.foundEntityName = params['riskyUser'];
 
-      this.filteredRiskyEntities = this.utilDataService.getFilteredRiskyUsers();
-    });
-  }
+            this.filteredRiskyEntities = this.utilDataService.getFilteredRiskyUsers();
+            this.filteredRiskyHosts = this.utilDataService.getFilteredRiskyHosts();
+            this.filteredRiskyIPAddress = this.utilDataService.getFilteredRiskyIPAddresses();
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    getRiskScoreColor(riskScore: number) {
+        if (riskScore <= 65) {
+            return 'limegreen';
+        } else if (riskScore > 65 && riskScore <= 79) {
+            return 'darkorange';
+        } else {
+            return "crimson";
+        }
+    }
 
 }

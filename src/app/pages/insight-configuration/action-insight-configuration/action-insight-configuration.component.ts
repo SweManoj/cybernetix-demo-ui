bc5acc5cf90e05e1b7ce1af77f2c424f710410df
sachinshetty824 <51442183+sachinshetty824@users.noreply.github.com>
2019-12-14 16:32:26 +0530

    insight action page basic design

--- src/app/pages/insight-configuration/action-insight-configuration/action-insight-configuration.component.ts
+++ src/app/pages/insight-configuration/action-insight-configuration/action-insight-configuration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-action-insight-configuration',
@@ -14,14 +15,40 @@ export class ActionInsightConfigurationComponent implements OnInit {
   viewInsightConfig = false;
   insightConfigId: number;
   pageTitle = 'Add Insight Configuration';
+  addPage = true;
+
+  optionsSelect: Array<any> = [
+    { value: '1', label: 'Option 1' },
+    { value: '2', label: 'Option 2' },
+    { value: '3', label: 'Option 3' },
+  ];
+
+  insightConfForm: FormGroup;
+  insightName: AbstractControl;
+  insightDescription: AbstractControl;
+  author: AbstractControl;
+
+  initInsightConfForm() {
+    this.insightConfForm = this.formBuilder.group({
+      insightName: ['', Validators.compose([Validators.required])],
+      insightDescription: ['', Validators.compose([Validators.required])],
+      author: ['']
+    });
+
+    this.insightName = this.insightConfForm.controls['insightName'];
+    this.insightDescription = this.insightConfForm.controls['insightDescription'];
+    this.author = this.insightConfForm.controls['author'];
+  }
 
   constructor(private activeRoute: ActivatedRoute, private router: Router,
-    private location: Location) {
+    private location: Location, private formBuilder: FormBuilder) {
+    this.initInsightConfForm();
   }
 
   ngOnInit() {
     const url = this.router.url;
     if (!url.includes('add')) {
+      this.addPage = false;
       this.activeRoute.paramMap.subscribe((params: Params) => {
         this.insightConfigId = params.get('insightConfId');
         this.pageTitle = (url.includes('edit') ? 'Edit ' : 'View ') + 'Insight Configuration';
--- src/app/pages/insight-configuration/insight-configuration.module.ts
+++ src/app/pages/insight-configuration/insight-configuration.module.ts
@@ -5,6 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { ActionInsightConfigurationComponent } from './action-insight-configuration/action-insight-configuration.component';
 import { InsightConfigurationListComponent } from './insight-configuration-list/insight-configuration-list.component';
+import { CardModule } from 'primeng/card';
+import { MaterialModule } from '../../material';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   imports: [
